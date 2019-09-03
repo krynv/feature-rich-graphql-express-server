1473e408ab2455e7ba0a10194c0d7aff9ee886ae
VK <vitaliy.krynytskyy@gmail.com>
2019-09-03 16:44:51 +0100

    Added PostgreSQL connectivity
    - Require a `.env` file to be added with the following format:
    
    ```
    DATABASE=postgres
    DATABASE_USER=postgres
    DATABASE_PASSWORD=postgres
    ```

--- src/models/index.js
+++ src/models/index.js
@@ -1,30 +1,26 @@
-let messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        dialect: 'postgres',
     },
-    2: {
-        id: '2',
-        text: 'Bye World',
-        userId: '2',
-    },
+);
+
+const models = {
+    User: sequelize.import('./user'),
+    Message: sequelize.import('./message'),
 };
 
-let users = {
-    1: {
-        id: '1',
-        username: 'John Appleseed',
-        messageIds: [1],
-    },
-    2: {
-        id: '2',
-        username: 'Chuck Norris',
-        messageIds: [2],
-    },
-};
+Object.keys(models).forEach(key => {
 
-export default {
-    users,
-    messages,
-}
+    if ('associate' in models[key]) {
+        models[key].associate(models);
+    }
+});
+
+export { sequelize };
+    
+export default models;

a9ca1f54c68ff01b82011fc817d7aed0253ea2f4
VK <vitaliy.krynytskyy@gmail.com>
2019-09-03 12:01:21 +0100

    Added babel

--- src/index.js
+++ src/index.js
@@ -8,19 +8,36 @@ app.use(cors()); // enable cors
 const schema = gql`
   type Query {
     me: User
+    user(id: ID!): User
   }
 
   type User {
+    id: ID!
     username: String!
   }
+
 `;
+
+let users = {
+    1: {
+        id: '1',
+        username: 'John Appleseed',
+    },
+    2: {
+        id: '2',
+        username: 'Chuck Norris',
+    },
+};
+
+const me = users[1];
 
 const resolvers = {
     Query: {
+        user: (parent, { id }) => {
+            return users[id];
+        },
         me: () => {
-            return {
-                username: 'John Appleseed',
-            };
+            return me;
         },
     },
 };

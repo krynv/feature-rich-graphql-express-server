a9bd432a0c0a3aa2759be7fd8d34dfea58e6281a
VK <vitaliy.krynytskyy@gmail.com>
2019-09-05 10:18:14 +0100

    Updated the resolvers to use PostgreSQL

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 This solution uses [PosgreSQL](https://www.postgresql.org/) for persistence of data.
 
+## Prerequisites
+
+As PostgreSQL maintains the persistence of data, you will need to create a `.env` file in order to connect to your database.
+
+The `.env` file should look something like this:
+
+```
+DATABASE=postgres
+DATABASE_USER=postgres
+DATABASE_PASSWORD=postgres
+HOST=localhost
+```
+
+and must be placed in the root of this repository.
+
+
+* src/
+* node_modules/
+.babelrc
+<b>.env</b>
+.gitattributes
+.gitignore
+README.md
+package-lock.json
+package.json
+
+
 ## Run the application
 
 Install dependencies:
--- src/index.js
+++ src/index.js
@@ -7,23 +7,61 @@ import resolvers from './resolvers';
 import models, { sequelize } from './models';
 
 const app = express();
+const eraseDatabaseOnSync = true;
 
 app.use(cors()); // enable cors
 
 const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
-    context: {
+    context: async () => ({
         models,
-    },
+        me: await models.User.findByLogin('cnorris'),
+    }),
 });
 
 server.applyMiddleware({ app, path: '/graphiql' });
 
-sequelize.sync().then(async () => {
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+    if (eraseDatabaseOnSync) {
+        createUsersWithMessages();
+    }
 
     app.listen({ port: 8000 }, () => {
         console.log('Apollo Server on: http://localhost:8000/graphiql');
     });
-
 });
+
+const createUsersWithMessages = async () => {
+
+    await models.User.create(
+        {
+            username: 'jappleseed',
+            messages: [
+                {
+                    text: 'Created Apple',
+                },
+            ],
+        },
+        {
+            include: [models.Message],
+        },
+    );
+
+    await models.User.create(
+        {
+            username: 'cnorris',
+            messages: [
+                {
+                    text: 'Counted to infinity. Twice',
+                },
+                {
+                    text: 'Can speak braille',
+                },
+            ],
+        },
+        {
+            include: [models.Message],
+        },
+    );
+};
